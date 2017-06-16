7bc0b9b7d0dc48e6dfaee8edf81a8c3e22762178
Cavan Page <Cavan.Page@corp.int>
2017-06-16 14:14:19 -0700

    stable version working with tone.js output synth in browser. ability to change octave and simplify chord notes. mousedown, mouseup, and mouseout events on note pads.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,61 @@
 import { Component } from '@angular/core';
 import tonal from 'tonal'
+import tone from 'tone'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  
 })
 
 export class AppComponent {
   constructor(){
-    this.selectedChordType = "Major"
+    this.selectedChordType = "M"
+    this.synth = new tone.PolySynth(3, tone.Synth).toMaster();
   }
 
-  title = 'chord xperiment';
-  header = "select a root chord";
+  title = '';
+  header = "";
   
-  RootNotes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-  ChordTypes = ["Major", "minor", "7", "M7", "m7"]
+  RootNotes = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];//wait for tonal v1.0.0 for new method to return this array tonal.names()
+  ChordTypes = tonal.chord.names();
 
-  chordProgression = [];
+  chordProgression: Chord[] = [];
   
-  chordDisplay;
-  selectedChordType;
-
-  addToChordProgression(chord: Chord){
+  chordNotesOctave: string[]; //all notes w/ specified octave
+  selectedChordType: string;
+  octave: number = 3;
+ 
+  synth: tone;
+  
+  addToChordProgression(note){
+    var chord = new Chord(note, this.selectedChordType, 4 );
     this.chordProgression.push(chord);    
   }
 
-  displayChord(note){    
-    this.chordDisplay = tonal.chord(note+this.selectedChordType)
-    console.log(tonal.chord.names())
+  playChord(note){
+    var octave =  this.octave;
+    var chordNotes = tonal.chord(note+this.selectedChordType);
+    this.chordNotesOctave = [];
+
+    //simplify each note (ex. F## -> G ) 
+    for(var i =0; i< chordNotes.length; i++){
+        chordNotes[i] = tonal.note.simplify(chordNotes[i])
+    }
+
+    //set the octave for each note
+    for(var i = 0; i < chordNotes.length; i++){
+      if(i > 0 && (this.RootNotes.indexOf(chordNotes[i]) < this.RootNotes.indexOf(chordNotes[i-1]))){
+        octave++;
+      }
+      this.chordNotesOctave[i] = chordNotes[i] + octave
+    }
+
+    this.synth.triggerAttack(this.chordNotesOctave);
+  }
+
+  stopChord(){
+    this.synth.triggerRelease(this.chordNotesOctave);
   }
 }
 
